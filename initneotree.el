dddf7df53e638d680b26afa29ad8d7bf2ce58d7c
zhonghua <secondsquare@qq.com>
2025-08-15 17:09:31 +0800

    use f12 to toggle neotree

---
+++ initneotree.el
@@ -0,0 +1,27 @@
+(require 'neotree)
+
+(setq neo-smart-open t)
+
+(defvar tab-bar-mode-state-on-neotree nil)
+
+(defun neotree-tab-toggle ()
+	(interactive)
+	(let ((old-tab-bar-mode tab-bar-mode))
+
+		(when tab-bar-mode
+      (tab-bar-new-tab -1)
+			(tab-bar-mode -1))
+
+		(neotree-toggle)
+
+		(when (and (not (neo-global--window-exists-p))
+							 tab-bar-mode-state-on-neotree)
+			(tab-bar-mode 1))
+
+		(setq tab-bar-mode-state-on-neotree old-tab-bar-mode)))
+
+
+(global-set-key [f12] 'neotree-tab-toggle)
+(global-set-key [f11] 'tab-bar-mode)
+
+(provide 'initneotree)
